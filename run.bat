70b86561deb42489ea9fb753f56d291a7ddf9b1b
Jyoti Patial <jasminethakur.joy1@gmail.com>
2024-11-20 20:32:18 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,1 @@
-python get-pip.py
-pip install pytest
-pip install selenium
-pip install pytest-html
-pip install pytest-xdist
-pip install openpyxl
-pip install allure-pytest
 pytest -v -s testcase\test_001_accountregistration.py --browser chrome
